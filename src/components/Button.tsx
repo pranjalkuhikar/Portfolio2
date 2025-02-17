fad2ef5451b6e9841417846f89ebf7fd215f0e57
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-02-17 22:35:10 +0530

    enhance Button component with hover effect and updated styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,9 @@ interface ButtonProps {
 
 const Button = ({ text }: ButtonProps) => {
   return (
-    <div className="px-3 py-2 bg-[#DAC5A7] text-[#1D1E1F] font-light tracking-widest uppercase">
+    <div className="px-3 py-2 bg-[#DAC5A7] text-[#1D1E1F] font-normal text-sm tracking-widest uppercase cursor-pointer flex flex-col group">
       {text}
+      <span className="w-0 group-hover:w-full bg-[#1D1E1F] h-[1px] rounded-xs transition-all duration-300"></span>
     </div>
   );
 };
